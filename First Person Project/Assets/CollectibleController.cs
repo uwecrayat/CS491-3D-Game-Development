462a5594ddf6d846847fd3b28cbd97bf45a219a5
uwecrayat <rayat@uwec.edu>
2016-02-23 14:20:42 -0600

    wip hud

---
+++ First Person Project/Assets/CollectibleController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectibleController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+   public void Acquire()
+    {
+        Destroy(gameObject);
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ First Person Project/Assets/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    public GameObject canvas;
+    private GameObject objectToTake;
+	// Use this for initialization
+	void Start () {
+	
+	}
+    private void ShowUI(bool isEnabled)
+    {
+        canvas.SetActive(isEnabled);
+        GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = !isEnabled;
+        Cursor.lockState = isEnabled ? CursorLockMode.None : CursorLockMode.Locked;
+        Cursor.visible = isEnabled;
+    }
+
+    public void OnTake()
+    {
+        objectToTake.GetComponent<CollectibleController>().Acquire();
+        ShowUI(false);
+    }
+
+    public void OnLeave()
+    {
+        ShowUI(false);
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        objectToTake = collider.gameObject;
+        ShowUI(true);
+    }
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
