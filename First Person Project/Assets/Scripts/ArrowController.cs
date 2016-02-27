7f66d7cc2f15130adf50a9d39f19ae2b85fd9c19
uwecrayat <rayat@uwec.edu>
2016-02-26 19:39:15 -0600

    On screen 3D arrow points to closest collectible

---
+++ First Person Project/Assets/Scripts/ArrowController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowController : MonoBehaviour {
+    public Transform target;
+    private Vector3 toCollectible;
+    private GameObject nearestCollectible;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float nearestSquaredDistance = Mathf.Infinity;
+        foreach (GameObject collectible in GameObject.FindGameObjectsWithTag("Collectible"))
+        {
+            toCollectible = collectible.transform.position - transform.position;
+            float squaredDistance = toCollectible.sqrMagnitude;
+            if (squaredDistance < nearestSquaredDistance)
+            {
+                nearestCollectible = collectible;
+                nearestSquaredDistance = squaredDistance;
+            }
+        }
+        Vector3 relativePos = nearestCollectible.transform.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(relativePos);
+        transform.rotation = rotation;
+    }
+}
