0edfab08e1113ee6d96be2d81742df2405a3e337
uwecrayat <rayat@uwec.edu>
2016-02-28 19:32:25 -0600

    win condition is needed

--- First Person Project/Assets/Scripts/PlayerController.cs
+++ First Person Project/Assets/Scripts/PlayerController.cs
@@ -6,13 +6,18 @@ public class PlayerController : MonoBehaviour {
     public GameObject canvas;
     public GameObject arrow;
     public GameObject[] buttons;
+    public Text hintText;
 
     private Stack moveList = new Stack();
     private GameObject objectToTake;
     private GameObject nearestCollectible;
     private Vector3 toCollectible;
+    private int numHints;
+    private float timeToNoHint;
+
 	// Use this for initialization
 	void Start () {
+        numHints = 3;
         for (int i = 0; i < buttons.Length; i++)
         {
             moveList.Push(buttons[i]);
@@ -22,6 +27,16 @@ public class PlayerController : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        hintText.text = "" + numHints;
+        if (Time.time > timeToNoHint)
+        {
+            if (Input.GetKeyDown(KeyCode.H) && !canvas.activeSelf && numHints > 0)
+            {
+               
+                StartCoroutine(showArrow());
+                timeToNoHint += 4;
+            }
+        }
         if (moveList.Count != 0)
         {
             Vector3 relativePos = (moveList.Peek() as GameObject).transform.position - arrow.transform.position;
@@ -29,7 +44,13 @@ public class PlayerController : MonoBehaviour {
             arrow.transform.rotation = rotation;
         }
     }
-
+    IEnumerator showArrow()
+    {
+        arrow.SetActive(true);
+        numHints--;
+        yield return new WaitForSeconds(4f);
+        arrow.SetActive(false);
+    }
     private void ShowUI(bool isEnabled)
     {
         print(moveList.Count);
